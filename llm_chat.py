0fd76704af4a8d95f1c8cfc71c65ca74a2f113cd
David Tam <davidtam@users.noreply.github.com>
2024-10-08 22:07:50 +0100

    a working example with readme that is just checklist of what needs to be done

--- llm_chat.py
+++ llm_chat.py
@@ -1,9 +1,37 @@
 import streamlit as st
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
+
+load_dotenv()
+
+MODEL_LIST = sorted(["gemma2", "llama3.2", "mistral-nemo", "qwen2.5"])
+
+
+def ask_question_sbs(question: str, model_name: str = "llama3.2") -> str:
+
+    template = """Question: {question}
+                  Answer: Let's think step by step."""
+    llm_prompt = ChatPromptTemplate.from_template(template)
+    model = OllamaLLM(model=model_name)
+    chain = llm_prompt | model
+    output = chain.invoke({"question": question})
+
+    print(f"{question=}")
+    print(f"{output=}")
+
+    return output
+
 
 if __name__ == "__main__":
 
+    print("Starting streamlit app...")
     st.title("Echo Bot")
+    option = st.selectbox(
+        "Please select who you want to chat to?",
+        MODEL_LIST,
+    )
 
     # Initialize chat history
     if "messages" not in st.session_state:
@@ -21,7 +49,9 @@ if __name__ == "__main__":
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = f"Echo: {prompt}"
+        output = ask_question_sbs(prompt, option)
+
+        response = f"{option}: {output}"
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             st.markdown(response)
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 Requires asdf
+
+ * [ ] write about installation and setup
+   * [ ] pre-requisite
+ * [ ] how to run the code that is already there
+ * [ ] setup in github and push
+ * 
